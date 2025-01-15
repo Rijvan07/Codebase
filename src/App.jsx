a0133410e50d35ae6daeb0df412e63639f9cf126
Rijvan07 <rijvanshaikh888@gmail.com>
2025-01-15 17:42:40 +0530

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <>
      <Routes>
         {/* <Route path='/' element={<Home/>} /> */}
-        <Route path='/forgotpasswordy' element={<ForgotPassword />} />
+        <Route path='/' element={<ForgotPassword />} />
       </Routes>
     </>
   )
